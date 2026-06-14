c13d493bf09dc2aa702f400b20771138a3a6c1ac
SalmaZango <r0996908@student.thomasmore.be>
2026-06-14 09:57:13 +0200

    toggle aanuit sound zaklamp

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -5,10 +5,21 @@ public class Flashlight : MonoBehaviour
     [SerializeField] GameObject FlashlightLight;
     private bool FlashlightActive = false;
 
+    [Header("Geluid Instellingen")]
+    [SerializeField] private AudioSource audioSource; // De speaker
+    [SerializeField] private AudioClip clickSound;    // Het geluidsbestand (.mp3/.wav)
+
     // Start is called before the first frame update
     void Start()
     {
         FlashlightLight.gameObject.SetActive(false);
+        
+        // Automatische check: als je de AudioSource vergeet te slepen, 
+        // zoekt hij of er eentje op hetzelfde object staat.
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
     }
 
     // Update is called once per frame
@@ -16,6 +27,12 @@ public class Flashlight : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
+            // Speel het klikgeluid af zodra je op F drukt!
+            if (audioSource != null && clickSound != null)
+            {
+                audioSource.PlayOneShot(clickSound);
+            }
+
             if (FlashlightActive == false)
             {
                 FlashlightLight.gameObject.SetActive(true);
