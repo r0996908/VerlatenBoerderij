7158188c49c9509684b006709f4ed11c8cc4b8b8
SalmaZango <r0996908@student.thomasmore.be>
2026-06-14 09:04:52 +0200

    zaklamp toggle

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] GameObject FlashlightLight;
+    private bool FlashlightActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FlashlightLight.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (FlashlightActive == false)
+            {
+                FlashlightLight.gameObject.SetActive(true);
+                FlashlightActive = true;
+            }
+            else
+            {
+                FlashlightLight.gameObject.SetActive(false);
+                FlashlightActive = false;
+            }
+        }
+    }
+}
